dcc1afcd8952779d4416eb4a295447ae601b4476
Seungjae Lee <mathlife12345@gmail.com>
2025-03-23 13:39:34 +0900

    인벤토리 페이지 셀을 선택하더라도 장비 장착 여부를 확인할 수 있도록 개선

--- Assets/Scripts/Gameplay/UI/Layer02 PageLayer/Page04 InventoryPage/InventoryMechPartGridViewCell.cs
+++ Assets/Scripts/Gameplay/UI/Layer02 PageLayer/Page04 InventoryPage/InventoryMechPartGridViewCell.cs
@@ -6,47 +6,39 @@ using UnityEngine.UI.Extensions;
 
 namespace Mathlife.ProjectL.Gameplay.UI
 {
-    public class InventoryMechPartGridViewCell 
+    public class InventoryMechPartGridViewCell
         : SimpleGridViewCell<MechPartModel, SimpleGridViewContext>
     {
         // View
         [SerializeField]
         private GameObject selectionGameObject;
-        
+
         [SerializeField]
         private GameObject equippedMarkGameObject;
-        
+
         [SerializeField]
         private UIEffect uiEffect;
-        
+
         [SerializeField]
         private Image iconImage;
 
         // Field
         private MechPartModel mechPart;
-        
+
         public override void UpdateContent(MechPartModel itemData)
         {
             mechPart = itemData;
-            
+
             if (mechPart == null)
             {
                 throw new ArgumentNullException("[InventoryMechPartGridViewCell] null 부품 데이터가 인자로 들어왔습니다.");
             }
-            
+
             uiEffect.LoadPreset(mechPart.Rarity.ToGradientPresetName());
             iconImage.sprite = mechPart.Icon;
 
-            if (Context.selectedIndex == Index)
-            {
-                selectionGameObject.SetActive(true);
-                equippedMarkGameObject.SetActive(false);
-            }
-            else
-            {
-                selectionGameObject.SetActive(false);
-                equippedMarkGameObject.SetActive(mechPart.Owner.Value != null);
-            }
+            equippedMarkGameObject.SetActive(mechPart.Owner.Value != null);
+            selectionGameObject.SetActive(Context.selectedIndex == Index);
         }
     }
 }
--- Assets/Scripts/Gameplay/UI/Layer02 PageLayer/Page04 InventoryPage/InventorySelectedMechPartView.cs
+++ Assets/Scripts/Gameplay/UI/Layer02 PageLayer/Page04 InventoryPage/InventorySelectedMechPartView.cs
@@ -11,16 +11,9 @@ namespace Mathlife.ProjectL.Gameplay.UI
         // View
         [SerializeField]
         private MechPartBasicView basicView;
-
-        [SerializeField]
-        private Button equipButton;
-        
-        [SerializeField]
-        private Button sellButton;
         
         // Field
         private MechPartModel mechPart;
-        private readonly CompositeDisposable disposables = new();
         
         public void Setup(MechPartModel pMechPart)
         {
@@ -30,14 +23,6 @@ namespace Mathlife.ProjectL.Gameplay.UI
         public override void Draw()
         {
             base.Draw();
-
-            equipButton.OnClickAsObservable()
-                .Subscribe(OnClickEquipButton)
-                .AddTo(disposables);
-            
-            sellButton.OnClickAsObservable()
-                .Subscribe(OnClickSellButton)
-                .AddTo(disposables);
             
             basicView.Setup(mechPart);
             basicView.Draw();
@@ -46,24 +31,7 @@ namespace Mathlife.ProjectL.Gameplay.UI
         public override void Clear()
         {
             base.Clear();
-            disposables.Clear();
             basicView.Clear();
-        }
-
-        private void OnDestroy()
-        {
-            disposables.Dispose();
-        }
-        
-        // 이벤트 처리
-        private void OnClickEquipButton(Unit _)
-        {
-            Debug.Log("Equip!");
-        }
-        
-        private void OnClickSellButton(Unit _)
-        {
-            Debug.Log("Sell!");
         }
     }
 }
