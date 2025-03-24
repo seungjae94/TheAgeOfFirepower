909d492a13b98e400f1a5a1459be04b6699c8568
Seungjae Lee <mathlife12345@gmail.com>
2025-03-24 17:09:53 +0900

    스테이지 선택 페이지 기본 레이아웃 구성

---
+++ Assets/Scripts/Gameplay/UI/Layer02 PageLayer/Page06 StageSelectionPage/StageSelectionPage.cs
@@ -0,0 +1,30 @@
+﻿using Mathlife.ProjectL.Utils;
+using TMPro;
+using UniRx;
+using UnityEngine.UI;
+
+namespace Mathlife.ProjectL.Gameplay.UI
+{
+    public class StageSelectionPage : Page
+    {
+        public override string PageName => "전투";
+        
+        private readonly CompositeDisposable disposables = new();
+        
+        protected override void OnOpen()
+        {
+            // Overlay
+            Find<NavigateBackOverlay>().Activate();
+        }
+
+        protected override void OnClose()
+        {
+            disposables.Clear();
+        }
+
+        private void OnDestroy()
+        {
+            disposables.Dispose();
+        }
+    }
+}
